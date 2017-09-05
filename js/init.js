13558532601d375b3fc62d2e24d5a07d7cd65986
Fábio Marquesim Ramos <marquesim.fabio@gmail.com>
2017-09-05 14:44:28 -0300

    V 1.3
    - Componentes adicionados ao Index
    - Arquivo init.js criado com Parallax e menu hamburger

---
+++ js/init.js
@@ -0,0 +1,9 @@
+// JS INIT
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
