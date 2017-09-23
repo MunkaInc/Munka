e66fc76d216d91a238e559641e2cd1c04789d0d6
Fábio Marquesim Ramos <marquesim.fabio@gmail.com>
2017-09-23 20:28:56 -0300

    V 2.5
    - Edição do Controller

--- Munka_WebApp/src/java/com/br/munka/model/connection/SingleConnection.java
+++ Munka_WebApp/src/java/com/br/munka/model/connection/SingleConnection.java
@@ -5,10 +5,59 @@
  */
 package com.br.munka.model.connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author fabiomarquesim
  */
 public class SingleConnection {
+    //Parametros de conexao
+    public static final String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+    private static final String bancoNome = "MunkaDB";
+    private static final String usuario = "admMunka";
+    private static final String senha = "123";
+    private static final String protocolo = "jdbc:derby:";
+    private static final String host = "//localhost:1527/";
     
+    private static SingleConnection instance = null;
+    private Connection connection;
+
+    private SingleConnection() {
+        super();
+        System.out.println("Singleton da conexão criado!");
+
+        try {
+            //Estabelecer a conexao
+            Class.forName(driver).newInstance();
+            connection = DriverManager.getConnection(protocolo + host + bancoNome, usuario, senha);
+
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
+            Logger.getLogger(SingleConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static SingleConnection getInstance() {
+        if (instance == null) {
+            instance = new SingleConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void closeConnection(){
+        try {
+            this.connection.close();
+        } catch (SQLException ex) {
+            ex.getMessage();
+            ex.getErrorCode();
+        }
+    }
 }
